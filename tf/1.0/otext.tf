6608406bea99278b1b0fbd5f25ae236312623481
cbop-dev <ban33man-dev@yahoo.com>
2026-02-20 00:37:12 -0500

    Initial commit of WEB-C Text-Fabric dataset version 1.0

---
+++ tf/1.0/otext.tf
@@ -0,0 +1,9 @@
+@config
+@appName=web
+@fmt:text-orig-full={text} 
+@sectionFeatures=book,chapter,verse
+@sectionTypes=book,chapter,verse
+@version=1.0
+@writtenBy=Text-Fabric
+@dateWritten=2026-02-20T04:41:39+00:00Z
+
---
+++ tf/1.0/otype.tf
@@ -0,0 +1,11 @@
+@node
+@appName=web
+@valueType=str
+@version=1.0
+@writtenBy=Text-Fabric
+@dateWritten=2026-02-20T04:41:37+00:00Z
+
+1-1006952	word
+1006953-1007025	book
+1007026-1008353	chapter
+1008354-1043732	verse
