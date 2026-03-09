618a1dbb54c3143b98a4e5a0d3b7a395107b0259
cbop-dev <ban33man-dev@yahoo.com>
2026-03-08 20:24:36 -0400

    Version 1.1 with verse text corruption fix

---
+++ tf/1.1/otext.tf
@@ -0,0 +1,9 @@
+@config
+@appName=web
+@fmt:text-orig-full={text} 
+@sectionFeatures=book,chapter,verse
+@sectionTypes=book,chapter,verse
+@version=1.0
+@writtenBy=Text-Fabric
+@dateWritten=2026-03-09T00:21:47+00:00Z
+
---
+++ tf/1.1/otype.tf
@@ -0,0 +1,11 @@
+@node
+@appName=web
+@valueType=str
+@version=1.0
+@writtenBy=Text-Fabric
+@dateWritten=2026-03-09T00:21:45+00:00Z
+
+1-858772	word
+858773-858845	book
+858846-860173	chapter
+860174-895552	verse
